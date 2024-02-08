cc4a21de29114a6ad0afd66f043de63cd182cbb8
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-02-08 19:59:16 +0500

    minor change

--- src/components/benefits/Benefits.js
+++ src/components/benefits/Benefits.js
@@ -2,14 +2,18 @@ import React from "react";
 
 import './benefits.scss'
 
+import '../../assets/WOW/css/libs/animate.css'
+
+// new WOW().init();
+
 export default function Benefits(){
     return(
-        <div className="benefits">
+        <div className="benefits wow">
             <div className="component-title">
                 <h1>В чем наши преимущества?</h1>
-                <div className="scroller">
-                    <div className="scroller-wrapper">
-                        <div className="scroller-item">богатый опыт</div>
+                <div className="scroller wow">
+                    <div className="scroller-wrapper wow">
+                        <div className="scroller-item" >богатый опыт</div>
                         <div id="long-word-item" className="scroller-item">подготовка к 1C 8.3</div>
                         <div className="scroller-item" id="long-word-item">практическое обучение </div>
                         <div id="long-word-item" className="scroller-item" id="long-word-item">индивидуальный подход </div>

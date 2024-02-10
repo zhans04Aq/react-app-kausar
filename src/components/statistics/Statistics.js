3854a398d9d3dbf10ece36b90dbd720ea64d0a87
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-02-10 12:49:51 +0500

    minor changes

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -3,33 +3,21 @@ import React from "react";
 import './statistics.scss'
 
 import graphEl from '../../assets/img/statistics-graph-el.svg'
-import graphElp from '../../assets/img/statistics-graph-el.png'
+
+import circleStatisticsPulse from '../../assets/img/circle-statistics.png'
 
 export default function Statistics (){
     return(
         <div className="statistics">
             <div className="dekstop-statistics">
                 <h1>Мы в цифрах</h1>
-                <img src={graphElp} alt="" className="graph-element" />
+                <img src={graphEl} alt="" className="graph-element" />
                 <div className="statistics-content">
                     <div className="content-item">
-                        <div className="pulse">
-                            <h2>1200</h2>
-                        </div>
+                        <img src={circleStatisticsPulse} alt="" />
                         <p></p>
                     </div>
-                    <div className="content-item">
-                        <div className="pulse">
-                            <h2>8</h2>
-                        </div>
-                        <p></p>
-                    </div>
-                    <div className="content-item">
-                        <div className="pulse">
-                            <h2>99%</h2>
-                        </div>
-                        <p></p>
-                    </div>
+
                 </div>
             </div>
             <div className="mobile-statistics"></div>

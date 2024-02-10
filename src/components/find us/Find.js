b8d6ecf5edd908e7c622c141d246ecf838a13a4a
Amankos Ansar <zugzwanggg0@gmail.com>
2024-02-10 22:39:35 +0500

    changes

---
+++ src/components/find us/Find.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import './find.scss'
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+
+import locationIcon from "../../assets/icons/location-icon.svg"
+import phoneIcon from "../../assets/icons/phone-icon.svg"
+import emailIcon from "../../assets/icons/email-icon.svg"
+
+export default function Find() {
+  return (
+    <div className='find'>
+      <div className='desktop-container'>
+        <h1 className='title'>Где вы можете найти нас</h1>
+        <div className='content'>
+          <div className='content-info'>
+            <ul className='info-list'>
+              <li className='info-item'>
+                <div className='item-head'>
+                  <span className='head-title'>
+                    Местоположение
+                  </span>
+                  <span>
+                    <img src={locationIcon}/>
+                  </span>
+                </div>
+                <p className='text'>
+                  г. Алматы, ул. Гоголя 86, БЦ КазЖол, 2 этаж, 14 офис
+                </p>
+              </li>
+
+              <li className='info-item'>
+                <div className='item-head'>
+                  <span className='head-title'>
+                    ТЕЛЕФОН
+                  </span>
+                  <span>
+                    <img src={phoneIcon}/>
+                  </span>
+                </div>
+                <p className='text'>
+                  +7 (747) 555-23-04
+                </p>
+              </li>
+
+              <li className='info-item'>
+                <div className='item-head'>
+                  <span className='head-title'>
+                    ПОЧТА
+                  </span>
+                  <span>
+                    <img src={emailIcon}/>
+                  </span>
+                </div>
+                <p className='text'>
+                  kausarconsult@gmail.com
+                </p>
+              </li>
+
+            </ul>
+
+            <div className='info-face'>
+              <b>Контакное лицо</b>
+              <p>Туриспаев Ержан Молдагалиев</p>
+            </div>
+            
+            
+          </div>
+          
+          
+          
+          <YMaps>
+            <Map style={{width: "750px", height: "780px",position: "absolute", right: 0 }} defaultState={{ center: [43.259088, 76.936918], zoom: 15 }}>
+              <Placemark geometry={[43.259088, 76.936918]}/>
+            </Map>
+          </YMaps>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./footer.scss"
+
+import visaLogo from "../../assets/img/visaLogo.png"
+import masterCardLogo from "../../assets/img/masterCardLogo.png"
+import logo from "../../assets/icons/logo-kausar.png"
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className='desktop-container'>
+        <div className='footer-top'>
+          <img src={visaLogo}/>
+          <img src={masterCardLogo}/>
+        </div>
+        <div className='footer-body'>
+          <div className='body-info'>
+            <h1>Все права защищены ©</h1>
+            <p>ТОО «Kausar Business Consulting»</p>
+            <p>БИН 150840022633</p>
+            <p>Юридический адресс: г. Алматы, Алмалинский район, ул. Гоголя, 86, офис 217</p>
+            <p>09:00 - 20:00, пн - пт</p>
+          </div>
+          <img src={logo}/>
+        </div>
+      </div>
+    </footer>
+  )
+}

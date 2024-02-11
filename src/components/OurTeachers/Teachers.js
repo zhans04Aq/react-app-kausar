25cb5945bf6233450219f2e6f5711a33bfa1bae5
Amankos Ansar <zugzwanggg0@gmail.com>
2024-02-11 16:27:31 +0500

    removed title according to design

--- src/components/OurTeachers/Teachers.js
+++ src/components/OurTeachers/Teachers.js
@@ -7,12 +7,21 @@ export default function Teachers () {
   return (
     <div className='teachers'>
       <div className='desktop-container'>
-        <h1 className='title'>Наши преподаватели</h1>
+        {/* <h1 className='title'>Наши преподаватели</h1> */}
         <ul className='teachers-list'>
           <li className='teacher-card'>
             <img src={teacherIcon} alt='Туриспаев Ержан Молдагалиевич'/>
             <p>
               Туриспаев Ержан Молдагалиевич
+            </p>
+            <span>
+              преподаватель
+            </span>
+          </li>
+          <li className='teacher-card'>
+            <img src={teacherIcon} alt='Туриспаев Ержан Молдагалиевич'/>
+            <p>
+              Байгазинова Дана Кыдырбековна
             </p>
             <span>
               преподаватель

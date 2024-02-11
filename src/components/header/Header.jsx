baaab6cb1624ccd3ff74c1666ef1a14026b3f617
Amankos Ansar <zugzwanggg0@gmail.com>
2024-02-11 17:41:50 +0500

    fixed header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,18 +3,21 @@ import React from "react";
 import './header.scss'
 
 import graphEl from '../../assets/img/header-graph-el.svg'
+import gradientBg from "../../assets/img/header-gradient-bg.png"
 
 const Header = () =>{
     return(
-        <header>
-            <img src={graphEl} alt="" className="header-graph-el" />
-            <div className="header-content">
-                <h1>Обучим професии ‘Бухгалтер’ с нуля!</h1>
-                <p>Пройдите полную подготовку в <span> Kausar <br />
-                Business Consulting</span> и станьте специалистом <br /> Бухгалтером! </p>
-                <div className="button-container">
-                    <button>Записаться</button>
-                </div>
+        <header style={{backgroundImage: `url(${gradientBg})`}}>
+            
+            <div className="graph" style={{backgroundImage: `url(${graphEl})`}}>
+              <div className="header-content">
+                  <h1>Обучим професии ‘Бухгалтер’ с нуля!</h1>
+                  <p>Пройдите полную подготовку в <span> Kausar <br />
+                  Business Consulting</span> и станьте специалистом <br /> Бухгалтером! </p>
+                  <div className="button-container">
+                      <button>Записаться</button>
+                  </div>
+              </div>
             </div>
         </header>
     )

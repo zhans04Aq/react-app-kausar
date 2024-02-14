d373628b3f041f3418a8747baeba3276bd3181f8
Amankos Ansar <zugzwanggg0@gmail.com>
2024-02-14 20:53:04 +0500

    teacher responsive

--- src/components/OurTeachers/Teachers.jsx
+++ src/components/OurTeachers/Teachers.jsx
@@ -9,7 +9,7 @@ export default function Teachers () {
   return (
     <div className='teachers'>
       <div className='container'>
-        {/* <h1 className='title'>Наши преподаватели</h1> */}
+        {/* <h1 className='title-primary'>Наши преподаватели</h1> */}
         <ul className='teachers-list'>
           <li className='teacher-card'>
             <img src={BaygzinovnaImg} alt='Туриспаев Ержан Молдагалиевич'/>

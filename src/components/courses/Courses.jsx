a39b2e20f0d063af2b9cf65b86d6128d4ce3bcea
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-02-15 23:01:18 +0500

    completed changes for courses component

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -16,13 +16,72 @@ export default function Courses(){
                                 <h1>Форма <br /> 910, ЭСФ</h1>
                             </div>
                             <div className="center-content">
-                                <p>70 000 KZT</p>
+                                <p>11.990 KZT</p>
                             </div>
                             <div className="lower-content">
                                 <button><a href="https://wa.me/77758281024">купить</a></button>
                                 <button><a href="https://wa.me/77758281024"><img src={arrow} alt="" className="arrow" /></a></button>
                             </div>
                         </div>
+                    </div>
+                    <div className="card" id="card-2">
+                        <div className="card-content">
+                            <div className="upper-content">
+                                <h1>Бухгалтерия в <br /> компанию</h1>
+                            </div>
+                            <div className="center-content">
+                                <p>189,990 KZT</p>
+                            </div>
+                            <div className="lower-content">
+                                <button><a href="https://wa.me/77758281024">купить</a></button>
+                                <button><a href="https://wa.me/77758281024"><img src={arrow} alt="" className="arrow" /></a></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card" id="card-3">
+                        <div className="card-content">
+                            <div className="upper-content">
+                                <h1>Бухгалтер <br /> для ИП</h1>
+                            </div>
+                            <div className="center-content">
+                                <p>39,990 KZT</p>
+                            </div>
+                            <div className="lower-content">
+                                <button><a href="https://wa.me/77758281024">купить</a></button>
+                                <button><a href="https://wa.me/77758281024"><img src={arrow} alt="" className="arrow" /></a></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card" id="card-4">
+                        <div className="card-content">
+                            <div className="upper-content">
+                                <h1>Всеобщее <br /> декларирование</h1>
+                            </div>
+                            <div className="center-content">
+                                <p>19,990 KZT</p>
+                            </div>
+                            <div className="lower-content">
+                                <button><a href="https://wa.me/77758281024">купить</a></button>
+                                <button><a href="https://wa.me/77758281024"><img src={arrow} alt="" className="arrow" /></a></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card" id="card-5">
+                        <div className="card-content">
+                            <div className="upper-content">
+                                <h1>ЭСФ, СНТ и <br /> виртуальный <br /> склад</h1>
+                            </div>
+                            <div className="center-content">
+                                <p>19,990 KZT</p>
+                            </div>
+                            <div className="lower-content">
+                                <button><a href="https://wa.me/77758281024">купить</a></button>
+                                <button><a href="https://wa.me/77758281024"><img src={arrow} alt="" className="arrow" /></a></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card" id="card-6">
+                        <p>СКОРО...</p>
                     </div>
                 </div>
             </div>
